702f2f504d4e3e9518e3d0a57cd004e276ec15f7
mrityunjayshukla411 <mrityunjayshukla411@gmail.com>
2023-04-10 02:49:43 +0530

    first commit

--- pages/qna.js
+++ pages/qna.js
@@ -148,6 +148,7 @@ const QNA = () => {
                     autoStart: true,
                     loop: true,
                     delay: 20,
+                    key: question
                   }}
                 />
               )}
